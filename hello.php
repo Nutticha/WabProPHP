2d53838630867b7ddacac9e27eeed5f71b7ea44b
Nutticha <nutticha30234@gmail.com>
2021-01-13 09:58:17 +0700

    input hello.php

---
+++ hello.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>Hello</title>
+    </head>
+    <body>
+        
+         <?php
+           echo "<h1> Hello </h1><br>";
+           echo "<h2> Aytthaya </h2>";
+         
+         
+         ?>
+            
+         
+    
+    </body>
+</html>
